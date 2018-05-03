745d91356d98b6de1d07cdd5b1cf09bcf5be114e
tbinkasem <tbinkasem@gmail.com>
2018-05-03 15:49:01 +0700

    add authLogin page

---
+++ pages/authLogin.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 class="red">Login Page App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+
+<style scoped>
+.red{
+  color: blueviolet;
+}
+</style>
+
