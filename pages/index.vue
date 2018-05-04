6d38d8d603b89fb68a63ff36081da70342bccfad
tbinkasem <tbinkasem@gmail.com>
2018-05-04 17:09:32 +0700

    edit last index and stdList

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,12 @@
 <template>
   <div>
-    <h1>Volunteer Activity Application</h1>
+    <h1>กิจกรรมจิตอาสาสะสมความดี</h1>
   </div>
 </template>
+
 <script>
 export default {
-  data() {
-    return {
-      
-    }
-  },
+  
 }
 </script>
 
