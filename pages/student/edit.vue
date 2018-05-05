ab8d3e980828a01984949eeca69d14d3983190c7
tbinkasem <tbinkasem@gmail.com>
2018-05-05 14:40:21 +0700

    fix field name in db

--- pages/student/edit.vue
+++ pages/student/edit.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h1>Student Edit</h1>
-    <v-text-field v-model="firstName" label="ชื่อ" />
-    <v-text-field v-model="lastName" label="นามสกุล" />
+    <v-text-field v-model="fname" label="ชื่อ" />
+    <v-text-field v-model="lname" label="นามสกุล" />
     <v-btn @click="save">บันทึก</v-btn>
   </div>
 </template>
@@ -11,14 +11,14 @@
 export default {
   data() {
     return {
-      firstName: '',
-      lastName: '',
+      fname: '',
+      lname: '',
     }
   },
   async created() {
     let res = await this.$http.get('/student/id/' + this.$route.query.id)
-    this.firstName = res.data.student.firstName || ''
-    this.lastName = res.data.student.lastName || ''
+    this.fname = res.data.student.fname || ''
+    this.lname = res.data.student.lname || ''
     // {
     //   ok: true,
     //   student: {
@@ -34,8 +34,8 @@ export default {
     async save() {
       let res = await this.$http.post('/student/save', {
         id: this.$route.query.id,
-        firstName: this.firstName,
-        lastName: this.lastName,
+        fname: this.fname,
+        lname: this.lname,
       })
       if (!res.data.ok) {
         // TODO: แสดงข้อความ ว่าบันทึกไม่สำเร็จ
